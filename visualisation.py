696502049bae0811047b59ee1702999b046e55cb
Dhruvilbhatt <43905073+Dhruvilbhatt@users.noreply.github.com>
2022-01-26 21:00:20 +0530

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -15,10 +15,11 @@ with open('pixels_intensity_txt.txt') as f:
 
 fig, ax = plt.subplots()
 plt.rcParams.update({'font.size': 9})
+plt.xlabel('Pixel', fontsize = 16, fontweight="bold")
+plt.ylabel('Pixel', fontsize = 16, fontweight="bold")
 plt.imshow(intensity_data, cmap = 'viridis', extent = [0,dimnension,0,dimnension]) # Visualising the plasma image
 for label in (ax.get_xticklabels() + ax.get_yticklabels()):
     label.set_fontsize(13)
     label.set_fontweight("bold") 
-plt.xlabel('\n\n(a)', fontsize = 16, fontweight="bold")
 
 plt.show()
