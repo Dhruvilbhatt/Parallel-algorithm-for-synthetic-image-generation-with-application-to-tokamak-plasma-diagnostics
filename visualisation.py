e565910ac362e6ad550d35c5ad15ff7d76707c11
Dhruvilbhatt <43905073+Dhruvilbhatt@users.noreply.github.com>
2022-01-26 20:48:42 +0530

    Create visualisation.py

---
+++ visualisation.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+intensity_data = [] # Array to store the intensity data of each pixel
+
+dimnension = 0 # Dimension of the image
+
+with open('pixel_intensity_txt.txt') as f: 
+    for line in f:
+        intensity_data.append(float(line))
+
+    intensity_data = np.array(intensity_data)
+    dimnension = int(np.sqrt(len(intensity_data)))
+    intensity_data = intensity_data.reshape(dimnension, dimnension)
+
+fig, ax = plt.subplots()
+plt.rcParams.update({'font.size': 9})
+plt.imshow(intensity_data, cmap = 'viridis', extent = [0,dimnension,0,dimnension]) # Visualising the plasma image
+for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+    label.set_fontsize(13)
+    label.set_fontweight("bold") 
+plt.xlabel('\n\n(a)', fontsize = 16, fontweight="bold")
+
+plt.show()
